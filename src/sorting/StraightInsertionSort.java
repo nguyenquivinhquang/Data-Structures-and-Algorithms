661021d18f0d709f10ab8c4b6701d881f79ad8c7
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2021-04-07 21:27:14 +0700

    Update StraightInsertionSort.java

--- src/sorting/StraightInsertionSort.java
+++ src/sorting/StraightInsertionSort.java
@@ -1,31 +1,24 @@
-
 package sorting;
-
 import java.util.Comparator;
-
-/*
-Method: StraightInsertionSort
-Objective: Sort the array in descending or ascending
----
-1/ Space complexity: O(n)
-Reasons:
-    + To store the array
-Computational complexity:
-    + Best case: O(n), Average case O(n)
-    + At the current index i, this algo will find the i predecessor. If exist the predecessor that i is smaller that predecessor, it will continue go find the predecessor of predecessor.
-*/
 public class StraightInsertionSort<E> implements ISort<E> {
+    /* Method: sort
+    Objective: Sort an array of arbitrary type according to a comparator:
+    Computational complexity: O(n^2)
+        + Traversing through the array takes O(n), and in each iteration,
+        we compare the current element with the elements after it in the array,
+        which takes at most O(n) for each iteration. Thus, O(n^2). */
     @Override
     public void sort(E[] array, Comparator<E> comparator) {
-        int n = array.length;
-
-        for (int i = 1; i < n ; i++) {
-            for (int j = i - 1; j >= 0 && comparator.compare(array[j + 1], array[j ]) < 0; j-- ) {
-                E temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+        int current, walker;
+        E temp; current = 1;
+        while(current < array.length) {
+            temp = array[current];
+            walker = current - 1;
+            while((walker >= 0) && comparator.compare(temp, array[walker]) < 0) {
+                array[walker + 1] = array[walker]; //shift to right
+                walker -= 1;
             }
-
+            array[walker + 1] = temp;
+            current += 1;
         }
-    }
-}
+}}
