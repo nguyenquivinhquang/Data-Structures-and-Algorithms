d6041ca0a27e8d585db097b24fd66d33c38f1c59
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2021-04-07 23:21:19 +0700

    Update PointComparator.java

--- src/geom/PointComparator.java
+++ src/geom/PointComparator.java
@@ -1,30 +1,17 @@
 package geom;
-
 import java.util.Comparator;
-
 public class PointComparator implements Comparator<Point2D> {
     enum sortTyoe {
-        ASC(1),
-        DES(-1);
-
+        ASC(1), DES(-1);
         private int constant;
-
-        sortTyoe(int constant) {
-            this.constant = constant;
-        }
-
-        int getConstant() {
-            return this.constant;
-        }
+        sortTyoe(int constant) {this.constant = constant;}
+        int getConstant() {return this.constant;}
     }
-
     private sortTyoe direction;
-
     public PointComparator(boolean isAscending) {
         if (isAscending) this.direction = sortTyoe.ASC;
         else this.direction = sortTyoe.DES;
     }
-
     @Override
     public int compare(Point2D o1, Point2D o2) {
         if (Math.abs(o1.getX() - o2.getX()) < 1e-7) return 0;
