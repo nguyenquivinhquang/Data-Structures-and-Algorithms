414feb376d5d76498d6fde791435d3df619006de
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2021-04-08 00:12:20 +0700

    Update BubbleSort.java

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,19 +1,22 @@
-/*
- * Bubble sort is the worst since for each of the `n - 1` iterations
- * it always iterates through the array `n - i - 1` times where `i` is the
- * `i-th` iteration, which is more than all other algorithms since other
- * algorithms has a stop condition for the inner loop but Bubble sort does not.
- */
 package sorting;
 import java.util.Comparator;
 public class BubbleSort<E> implements ISort<E> {
-    @Override
-    public void sort(E arr[], Comparator<E> comparator) { 
-        int n = arr.length; 
-        for (int i = 0; i < n-1; i++) 
-            for (int j = 0; j < n-i-1; j++) 
-                if (comparator.compare(arr[j], arr[j + 1]) > 0) { 
-                    E temp = arr[j]; arr[j] = arr[j+1]; arr[j+1] = temp; 
-                } 
-    } 
+    public static void sort(E arr[], Comparator<E> comparator){
+        int current, walker;
+        boolean flag;
+        current = 0; 
+        flag = false;
+        while((current < arr.length-1) && (flag == false)){
+            walker = arr.length - 1; //start from the last and backward
+            flag = true; //for testing if the input already in ascending order
+            while(walker > current){
+                if(comparator.compare(arr[walker],arr[walker-1]) < 0){
+                    flag = false;
+		    E temp = arr[walker]; arr[walker] = arr[walker-1]; arr[walker-1] = temp; 
+                }
+                walker -= 1;
+            }
+            current += 1;
+        }
+    }
 }
