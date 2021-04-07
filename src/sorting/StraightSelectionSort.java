4d28efb25400dd9ca16ac0941992d25f35b51e80
Nguyễn Quí Vinh Quang <60070577+nguyenquivinhquang@users.noreply.github.com>
2021-04-07 21:24:43 +0700

    Update StraightSelectionSort.java

--- src/sorting/StraightSelectionSort.java
+++ src/sorting/StraightSelectionSort.java
@@ -1,24 +1,18 @@
 
 package sorting;
-
 import java.util.Comparator;
-
 /*
 Method: StraightInsertionSort
 Objective: Sort the array in descending or ascending
----
-1/ Space complexity: O(n)
-Reasons:
-    + To store the array
 Computational complexity: O(n^2),
-    + At iteration of the first loop, it will find the i th small element. To find that element, this algorithm will use another loop from i + 1 to n to find that small element.
+    + At iteration of the first loop, it will find the i th small element. To find that element,
+    this algorithm will use another loop from i + 1 to n to find that small element.
     + complexity is n * (n - 1) = n ^ 2
 */
 public class StraightSelectionSort<E> implements ISort<E> {
     @Override
     public void sort(E[] array, Comparator<E> comparator) {
         int n = array.length;
-
         // One by one move boundary of unsorted subarray
         for (int i = 0; i < n - 1; i++) {
             // Find the minimum element in unsorted array
